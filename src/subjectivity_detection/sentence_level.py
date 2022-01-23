4ea51cde8da185fc76faae7bcf6d84626f01745a
FarinaMatteo <matteo.farina-1@studenti.unitn.it>
2022-01-23 19:02:37 +0100

    parametrized sentence-level subjectivity detection

--- src/subjectivity_detection/sentence_level.py
+++ src/subjectivity_detection/sentence_level.py
@@ -13,7 +13,20 @@ repo_root = os.path.dirname(src_dir_path)
 from utils.preprocessing import load_corpus_rotten_imdb, get_movie_reviews_dataset, mr2str
 
 
-if __name__ == "__main__":
+def build_argparser():
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("-r", "--representation", type=str,
+                        default="count",
+                        help="The Document representation to train the subjectivity detector. Choose between: 'count' or 'tfidf'. Default 'count'.")
+    parser.add_argument("-clf", "--classifier", type=str, default="multinomial",
+                        help="The Naive Bayes classifier to train. Choose between 'multinomial' or 'bernoulli'. Default: 'multinomial'.")
+    return parser
+
+
+def main(representation="count", classifier="multinomial"):
+    assert representation in ("count", "tfidf")
+    assert classifier in ("multinomial", "bernoulli")
 
     # load the Rotten IMDB Dataset to train the subjectivity detector
     rotten_imdb_path = os.path.join(
@@ -29,25 +42,49 @@ if __name__ == "__main__":
     pos = mr2str(pos)
     mr = neg + pos
 
-    vectorizer = TfidfVectorizer()
+    # instantiate vectorizer and classifier based on passed arguments
+    if representation == "count":
+        vectorizer = CountVectorizer()
+    else:
+        vectorizer = TfidfVectorizer()
+
+    if classifier == "multinomial":
+        clf = MultinomialNB()
+    else:
+        clf = BernoulliNB()
+
+    # fit the vectorizer on the MovieReviews dataset to link it to its vocab
+    # MovieReviews will be the target dataset for the final evaluation!
     vectorizer.fit(mr)
+    
+    # then vectorize the RottenIMDB Dataset with the vocab constraints from MovieReviews 
     vectors = vectorizer.transform(dataset)
     labels = [1]*len(subj) + [0]*len(obj)
-    clf = MultinomialNB()
+
+    # perform cross validation and grab the best estimator
     scores = cross_validate(clf, vectors, labels,
                             cv=StratifiedKFold(n_splits=10),
                             scoring=['f1_micro'],
                             return_estimator=True,
                             n_jobs=-1)
-    average = sum(scores['test_f1_micro'])/len(scores['test_f1_micro'])
-    print("F1 Score: {:.3f}".format(average))
-
-    assert len(scores["estimator"]) == len(scores["test_f1_micro"])
     estimator = scores["estimator"][np.argmax(np.array(scores["test_f1_micro"]))]
+    
+    # display cross validation empirical results on the F1 metric
+    average = sum(scores['test_f1_micro'])/len(scores['test_f1_micro'])
+    print("Average F1 Score from cross validation: {:.2f}".format(average))
+    
+    # test the best estimator on the whole RottenIMBD dataset and display the output
     y_pred = estimator.predict(vectors)
     print(classification_report(labels, y_pred))
 
     # finally, dump the best estimator on disk
-    outpath = os.path.join(repo_root, 'models', 'sl_subjectivity_detector.joblib')
+    outpath = os.path.join(repo_root, 'models',
+                            f'{representation}_{classifier}_subj_det.joblib')
+    if not os.path.exists(os.path.dirname(outpath)):
+        os.makedirs(os.path.dirname(outpath))
     print("Saving model at: ", outpath)
     dump(estimator, outpath)
+
+if __name__ == "__main__":
+    parser = build_argparser()
+    main(**vars(parser.parse_args()))
